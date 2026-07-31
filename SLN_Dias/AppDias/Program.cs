e0b795448da8869ba117357a7bc585e2d33cff88
REGINALDO REIS DE SANTANA <113715701+reginaldosenai@users.noreply.github.com>
2026-07-31 15:17:24 -0300

    Corecao titulo

--- SLN_Dias/AppDias/Program.cs
+++ SLN_Dias/AppDias/Program.cs
@@ -7,7 +7,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("=================================");
-        Console.WriteLine("   SISTEMA DE GESTÃO DE TAREFAS EM A  ");
+        Console.WriteLine("   SISTEMA DE GESTÃO DE TAREFAS");
         Console.WriteLine("=================================\\n");
 
         Console.Write("Digite o nome da tarefa: ");
